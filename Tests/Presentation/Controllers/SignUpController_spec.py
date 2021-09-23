bed798c418d49f55d2c6b7f758ff3c57313412a1
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-09-23 20:49:39 -0300

    test: ensure SignUpController returns an exception if no email is provided

--- Tests/Presentation/Controllers/SignUpController_spec.py
+++ Tests/Presentation/Controllers/SignUpController_spec.py
@@ -3,15 +3,32 @@ from Core.Message import Message
 from Presentation.Controllers.SignUpController import SignUpController
 from Presentation.Errors.ValidationError import ValidationError
 
+
 def test_when_no_name_is_provided():
     """Should return an ValidationException if no name is provided """
     sut = SignUpController()
     message = Message({
-      'name': 'any_name',
       'email': 'any_email@mail.com',
       'password': 'any_password',
       'password_confirmation': 'any_password'
     })
 
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError) as result:
       sut.handle(message)
+
+    assert {'name': 'is required'} in result.value.errors
+
+
+def test_when_no_email_is_provided():
+    """Should return an ValidationException if no email is provided """
+    sut = SignUpController()
+    message = Message({
+      'name': 'any_name',
+      'password': 'any_password',
+      'password_confirmation': 'any_password'
+    })
+
+    with pytest.raises(ValidationError) as result:
+      sut.handle(message)
+
+    assert {'email': 'is required'} in result.value.errors
