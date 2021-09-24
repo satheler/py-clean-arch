269d4de18e0fa98bb3ef27ec18bdd274bb58f40f
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-09-23 21:29:03 -0300

    refactor: move system under test creation to a factory helper method

--- Tests/Presentation/Controllers/SignUpController_spec.py
+++ Tests/Presentation/Controllers/SignUpController_spec.py
@@ -3,10 +3,14 @@ from Core.Message import Message
 from Presentation.Controllers.SignUpController import SignUpController
 from Presentation.Errors.ValidationError import ValidationError
 
+def make_sut() -> SignUpController:
+  sut = SignUpController()
+
+  return sut
 
 def test_when_no_name_is_provided():
     """Should return an ValidationException if no name is provided """
-    sut = SignUpController()
+    sut = make_sut()
     message = Message({
       'email': 'any_email@mail.com',
       'password': 'any_password',
@@ -21,7 +25,7 @@ def test_when_no_name_is_provided():
 
 def test_when_no_email_is_provided():
     """Should return an ValidationException if no email is provided """
-    sut = SignUpController()
+    sut = make_sut()
     message = Message({
       'name': 'any_name',
       'password': 'any_password',
@@ -35,7 +39,7 @@ def test_when_no_email_is_provided():
 
 def test_when_no_password_is_provided():
     """Should return an ValidationException if no password is provided """
-    sut = SignUpController()
+    sut = make_sut()
     message = Message({
       'name': 'any_name',
       'email': 'any_email@mail.com',
@@ -49,7 +53,7 @@ def test_when_no_password_is_provided():
 
 def test_when_no_password_confirmation_is_provided():
     """Should return an ValidationException if no password_confirmation is provided """
-    sut = SignUpController()
+    sut = make_sut()
     message = Message({
       'name': 'any_name',
       'email': 'any_email@mail.com',
