5327f06a62a370ff87885800d85ed5876cbdc8e5
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-09-26 03:46:08 -0300

    refactor: move sut creation to a factory helper method

--- Tests/Utils/EmailValidatorAdapter_spec.py
+++ Tests/Utils/EmailValidatorAdapter_spec.py
@@ -3,10 +3,12 @@ from pytest_mock import MockerFixture
 
 from Utils.EmailValidatorAdapter import EmailValidatorAdapter
 
+def make_sut():
+  return EmailValidatorAdapter()
 
 def test_when_invalid_email_is_provided(mocker: MockerFixture):
     """Should return false if email is invalid"""
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     mocker.patch.object(sut, 'is_valid', return_value=False)
 
     is_valid = sut.is_valid('invalid_email@mail.com')
@@ -15,7 +17,7 @@ def test_when_invalid_email_is_provided(mocker: MockerFixture):
 
 def test_when_valid_email_is_provided(mocker: MockerFixture):
     """Should return true if email is valid"""
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     mocker.patch.object(sut, 'is_valid', return_value=True)
 
     is_valid = sut.is_valid('valid_email@mail.com')
@@ -23,7 +25,7 @@ def test_when_valid_email_is_provided(mocker: MockerFixture):
 
 def test_call_regex_match(mocker: MockerFixture):
     """Should return true if email is valid"""
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     re_spy = mocker.spy(re, 'match')
 
     email = 'valid_email@mail.com'
