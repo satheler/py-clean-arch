368ee6ed532588be908779fc25c0ea456c7202c0
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-09-25 21:39:56 -0300

    test: ensure raise if EmailValidator raises

--- Tests/Presentation/Controllers/SignUpController_spec.py
+++ Tests/Presentation/Controllers/SignUpController_spec.py
@@ -127,4 +127,25 @@ def test_call_email_validator_with_correct_values(mocker: MockerFixture):
 
     sut.handle(message)
 
-    email_validator_spy.assert_called_with(message.body.get('email'))
+    email_validator_spy.assert_called_once_with(message.body.get('email'))
+
+def test_raise_when_email_validator_raises(mocker: MockerFixture):
+    """Should raise if EmailValidator raises"""
+    test = make_sut()
+    sut = test.get('sut')
+    email_validator_stub = test.get('email_validator_stub')
+    
+    email_validator_spy = mocker.spy(email_validator_stub, 'is_valid')
+    email_validator_spy.side_effect = BaseException('Unexpected error')
+
+    message = Message({
+        'name': 'any_name',
+        'email': 'any_email@mail.com',
+        'password': 'any_password',
+        'password_confirmation': 'any_password'
+    })
+
+    with pytest.raises(BaseException):
+        sut.handle(message)
+
+    # assert {'email': 'is invalid'} in result.value.errors
