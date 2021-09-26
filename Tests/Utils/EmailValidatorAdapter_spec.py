93a7933edc079607ac026b1ca6a6f969e90cb6e0
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-09-26 03:45:10 -0300

    test: ensure EmailValidatorAdapter calls regex match with correct value

--- Tests/Utils/EmailValidatorAdapter_spec.py
+++ Tests/Utils/EmailValidatorAdapter_spec.py
@@ -1,4 +1,4 @@
-import pytest
+import re
 from pytest_mock import MockerFixture
 
 from Utils.EmailValidatorAdapter import EmailValidatorAdapter
@@ -20,3 +20,13 @@ def test_when_valid_email_is_provided(mocker: MockerFixture):
 
     is_valid = sut.is_valid('valid_email@mail.com')
     assert is_valid == True
+
+def test_call_regex_match(mocker: MockerFixture):
+    """Should return true if email is valid"""
+    sut = EmailValidatorAdapter()
+    re_spy = mocker.spy(re, 'match')
+
+    email = 'valid_email@mail.com'
+
+    sut.is_valid(email)
+    re_spy.assert_called_once_with('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$', email)
