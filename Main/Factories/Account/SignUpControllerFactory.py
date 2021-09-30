b5402d54ffd320da93b53525ea305eb9e559d79c
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-09-29 21:32:03 -0300

    feat: add SignUpController Factory

---
+++ Main/Factories/Account/SignUpControllerFactory.py
@@ -0,0 +1,16 @@
+from Infra.Repositories.Account.AccountCognitoRepository import AccountCognitoRepository
+from Data.UseCases.Account.IdentityProviderStoreAccount import IdentityProviderStoreAccount
+from Presentation.Controllers.SignUpController import SignUpController
+from Utils.EmailValidatorAdapter import EmailValidatorAdapter
+
+
+def make_sign_up_controller():
+    email_validator_adapter = EmailValidatorAdapter()
+    store_account_repository = AccountCognitoRepository()
+    identity_provider_store_account = IdentityProviderStoreAccount(
+        store_account_repository=store_account_repository
+    )
+    return SignUpController(
+        email_validator=email_validator_adapter,
+        store_account=identity_provider_store_account
+    )
